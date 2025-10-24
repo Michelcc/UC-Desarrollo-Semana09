d832edf5dea1736a40342af49a99535eae6eb165
Michelcc <76673533@continental.edu.pe>
2025-10-24 18:58:29 -0500

    Semana 10

---
+++ Assets/Script/ScriptsUI/UIState.cs
@@ -0,0 +1,14 @@
+public abstract class UIState
+{ 
+
+    protected UIManager m_uiManager;
+
+    public UIState (UIManager uiManager)
+    {
+    m_uiManager = uiManager;
+    }
+
+    public abstract void Enter();
+
+    public abstract void Exit();
+}
